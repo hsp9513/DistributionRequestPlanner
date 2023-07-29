fa2ffe10f5af18215d302ef3373e68e51fa0c12b
hsp9513 <hsp9513@naver.com>
2023-07-29 17:25:23 +0900

    guard exception

--- DistributionRequestPlanner/control.lua
+++ DistributionRequestPlanner/control.lua
@@ -36,153 +36,165 @@ end
 
 
 function on_player_reverse_selected_area(event) 
-    --game.print("on_player_reverse_selected_area")
-    if event.item=='item-requester' then
-        local player = game.players[event.player_index]
-        local setting = get_setting(player)
-        local item_requester = player.gui.left.item_requester
+    local success,message = pcall(function ()
+        --game.print("on_player_reverse_selected_area")
+        if event.item=='item-requester' then
+            local player = game.players[event.player_index]
+            local setting = get_setting(player)
+            local item_requester = player.gui.left.item_requester
 
-        if #event.entities == 0  then
-            return
-        end
-        if #event.entities > 1 then
-            --player.print("하나의 조립기계만 선택할 수 있습니다")
-            player.print({"distribution-request-planner.select-one"})
-            return
-        end
-
-        local target=event.entities[1]
-        local recipe = target.get_recipe()
-
-        if not recipe then
-            player.print({"distribution-request-planner.no-recipe"})
-            return
-        end
-
-        local ingredients = {}
-        local fluids = {}
-
-        for _,ingredient in pairs(recipe.ingredients) do
-            if ingredient.type=="item" then
-                --ingredient.amount = ingredient.amount * speed * 30 / recipe.energy
-                table.insert(ingredients, ingredient)
-            else
-                table.insert(fluids, ingredient)
+            if #event.entities == 0  then
+                return
             end
+            if #event.entities > 1 then
+                --player.print("하나의 조립기계만 선택할 수 있습니다")
+                player.print({"distribution-request-planner.select-one"})
+                return
+            end
+
+            local target=event.entities[1]
+            local recipe = target.get_recipe()
+
+            if not recipe then
+                player.print({"distribution-request-planner.no-recipe"})
+                return
+            end
+
+            local ingredients = {}
+            local fluids = {}
+
+            for _,ingredient in pairs(recipe.ingredients) do
+                if ingredient.type=="item" then
+                    --ingredient.amount = ingredient.amount * speed * 30 / recipe.energy
+                    table.insert(ingredients, ingredient)
+                else
+                    table.insert(fluids, ingredient)
+                end
+            end
+
+            setting.entity_name    = target.name
+            setting.crafting_speed = target.crafting_speed
+            setting.speed_bonus    = target.speed_bonus
+            setting.recipe         = recipe
+            setting.ingredients    = ingredients
+            setting.fluids         = fluids
+
+            update_gui(player)
+
+
+
+            --player.print("energy:"..recipe.energy)
+            --player.print(game.table_to_json(ingredients))
+
+            --if item_requester then
+            --    item_requester.top.entity_flow.entity_button.elem_value=target.name
+            --    local speed_bonus = (target.speed_bonus~=0) and (" ("..sign(toFixed(target.speed_bonus*100,2)).."%)") or ""
+            --    item_requester.top.entity_flow.entity_label.caption={"",target.localised_name,"\n","제작속도 : ",toFixed(target.crafting_speed,2), speed_bonus }
+            --    item_requester.top.recipe_flow.recipe_button.elem_value=recipe.name
+
+            --    local recipe_label = {"","재료 목록        ","\n[img=quantity-time]   "..recipe.energy}
+            --    for _,ingredient in pairs(ingredients) do
+            --        table.insert(recipe_label, "\n[img=item/"..ingredient.name.."] "..ingredient.amount)
+            --    end
+            --    for _,ingredient in pairs(fluids) do
+            --        table.insert(recipe_label, "\n[img=item/"..ingredient.name.."] "..ingredient.amount)
+            --    end
+            --    item_requester.top.recipe_flow.recipe_label.caption=recipe_label
+                
+            --    local request_label = {"","요청 목록","\n"}
+            --    local mul = tonumber(item_requester.top.n_count_flow.n_count_field.text)
+            --    for _,ingredient in pairs(ingredients) do
+            --        table.insert(request_label, "\n[img=item/"..ingredient.name.."] "..ingredient.amount*mul)
+            --    end
+            --    item_requester.top.recipe_flow.request_label.caption=request_label
+            --end
+            --global.target=ingredients
+
         end
-
-        setting.entity_name    = target.name
-        setting.crafting_speed = target.crafting_speed
-        setting.speed_bonus    = target.speed_bonus
-        setting.recipe         = recipe
-        setting.ingredients    = ingredients
-        setting.fluids         = fluids
-
-        update_gui(player)
-
-
-
-        --player.print("energy:"..recipe.energy)
-        --player.print(game.table_to_json(ingredients))
-
-        --if item_requester then
-        --    item_requester.top.entity_flow.entity_button.elem_value=target.name
-        --    local speed_bonus = (target.speed_bonus~=0) and (" ("..sign(toFixed(target.speed_bonus*100,2)).."%)") or ""
-        --    item_requester.top.entity_flow.entity_label.caption={"",target.localised_name,"\n","제작속도 : ",toFixed(target.crafting_speed,2), speed_bonus }
-        --    item_requester.top.recipe_flow.recipe_button.elem_value=recipe.name
-
-        --    local recipe_label = {"","재료 목록        ","\n[img=quantity-time]   "..recipe.energy}
-        --    for _,ingredient in pairs(ingredients) do
-        --        table.insert(recipe_label, "\n[img=item/"..ingredient.name.."] "..ingredient.amount)
-        --    end
-        --    for _,ingredient in pairs(fluids) do
-        --        table.insert(recipe_label, "\n[img=item/"..ingredient.name.."] "..ingredient.amount)
-        --    end
-        --    item_requester.top.recipe_flow.recipe_label.caption=recipe_label
-            
-        --    local request_label = {"","요청 목록","\n"}
-        --    local mul = tonumber(item_requester.top.n_count_flow.n_count_field.text)
-        --    for _,ingredient in pairs(ingredients) do
-        --        table.insert(request_label, "\n[img=item/"..ingredient.name.."] "..ingredient.amount*mul)
-        --    end
-        --    item_requester.top.recipe_flow.request_label.caption=request_label
-        --end
-        --global.target=ingredients
-
+    end)
+    if not success then
+        reset()
+        error_message(message)
     end
 end 
 
 function on_player_selected_area(event) 
-    --game.print("on_player_selected_area")
-    if event.item=='item-requester' then
-        local player = game.players[event.player_index]
-        local setting= get_setting(player)
-        local requests = setting.requests
+    local success,message = pcall(function ()
+        --game.print("on_player_selected_area")
+        if event.item=='item-requester' then
+            local player = game.players[event.player_index]
+            local setting= get_setting(player)
+            local requests = setting.requests
 
-        if #event.entities == 0  then
-            return
-        elseif #event.entities > 30 then
-            game.print({"distribution-request-planner.too-many-chest"})
-            return
-        end
+            if #event.entities == 0  then
+                return
+            elseif #event.entities > 30 then
+                game.print({"distribution-request-planner.too-many-chest"})
+                return
+            end
 
-        if #requests==0 then
-            player.print({"distribution-request-planner.no-request"})
-            return
-        end
+            if #requests==0 then
+                player.print({"distribution-request-planner.no-request"})
+                return
+            end
 
-        local requesters={}
-        for _,entity in pairs(event.entities) do
-           --game.print(entity.unit_number.. entity.prototype.logistic_mode )
-           if entity.prototype.logistic_mode == "requester" then
-               table.insert(requesters, entity)
-               --for slot=1, entity.request_slot_count do
-               --    entity.clear_request_slot(slot)
-               --end
-               if entity.request_slot_count>0 then
-                   player.print({"distribution-request-planner.already-set"})
-                   return
+            local requesters={}
+            for _,entity in pairs(event.entities) do
+               --game.print(entity.unit_number.. entity.prototype.logistic_mode )
+               if entity.prototype.logistic_mode == "requester" then
+                   table.insert(requesters, entity)
+                   --for slot=1, entity.request_slot_count do
+                   --    entity.clear_request_slot(slot)
+                   --end
+                   if entity.request_slot_count>0 then
+                       player.print({"distribution-request-planner.already-set"})
+                       return
+                   end
                end
-           end
-        end
+            end
 
-        local req_num = #requesters
-        player.print({"distribution-request-planner.selected-chest",req_num})
+            local req_num = #requesters
+            player.print({"distribution-request-planner.selected-chest",req_num})
 
-        --local toast_info = {}
-        for i=1,#requests do 
-            local request = requests[i]
-            local target_req_num = (i-1)%req_num+1
-            local requester = requesters[target_req_num]
-            --game.print("i="..i)
-            --game.print(game.table_to_json(request))
-            requester.set_request_slot({
-                name=request.name,
-                count=request.amount
-            },math.floor((i-1)/req_num)+1)
-            requester.request_from_buffers = setting.buffer_request
+            --local toast_info = {}
+            for i=1,#requests do 
+                local request = requests[i]
+                local target_req_num = (i-1)%req_num+1
+                local requester = requesters[target_req_num]
+                --game.print("i="..i)
+                --game.print(game.table_to_json(request))
+                requester.set_request_slot({
+                    name=request.name,
+                    count=request.amount
+                },math.floor((i-1)/req_num)+1)
+                requester.request_from_buffers = setting.buffer_request
 
-            --if not toast_info[target_req_num] then 
-            --    toast_info[target_req_num] = {
-            --        entity=requester,
-            --        items={}
+                --if not toast_info[target_req_num] then 
+                --    toast_info[target_req_num] = {
+                --        entity=requester,
+                --        items={}
+                --    }
+                --end
+                --table.insert(toast_info[target_req_num].items, "[item="..ingredient.name.."]")
+            end
+            --for _,info in pairs(toast_info) do
+            --    local entity = info.entity
+            --    local items = info.items
+            --    local surface = entity.surface
+            --    surface.create_entity{
+            --        name="flying-text",
+            --        position=entity.position,
+            --        text=table.concat(items,""),
+            --        render_player_index=event.player_index
             --    }
             --end
-            --table.insert(toast_info[target_req_num].items, "[item="..ingredient.name.."]")
+
+
         end
-        --for _,info in pairs(toast_info) do
-        --    local entity = info.entity
-        --    local items = info.items
-        --    local surface = entity.surface
-        --    surface.create_entity{
-        --        name="flying-text",
-        --        position=entity.position,
-        --        text=table.concat(items,""),
-        --        render_player_index=event.player_index
-        --    }
-        --end
-
-
+    end)
+    if not success then
+        reset()
+        error_message(message)
     end
 end
 
@@ -321,60 +333,79 @@ function hide_gui (player)
 end
 
 function on_player_cursor_stack_changed(event) 
-    local player = game.players[event.player_index]
-    if player.cursor_stack and player.cursor_stack.valid and player.cursor_stack.valid_for_read and player.cursor_stack.name=="item-requester" then
-        show_gui(player)
-    else
-        hide_gui(player)
+    local success,message = pcall(function ()
+
+        local player = game.players[event.player_index]
+        if player.cursor_stack and player.cursor_stack.valid and player.cursor_stack.valid_for_read and player.cursor_stack.name=="item-requester" then
+            show_gui(player)
+        else
+            hide_gui(player)
+        end
+    end)
+    if not success then
+        reset()
+        error_message(message)
     end
 
 
 end
 
 function on_gui_click(event)
-    local player_index = event.player_index
-    local player = game.players[player_index]
-    local item_requester = player.gui.left.item_requester
-    if not item_requester then return end
+    local success,message = pcall(function ()
+        local player_index = event.player_index
+        local player = game.players[player_index]
+        local item_requester = player.gui.left.item_requester
+        if not item_requester then return end
 
-    local setting = get_setting(player)
+        local setting = get_setting(player)
 
-    local element = event.element
-    if element and element.valid then
-        if element.name=="n_second_radio" or element.name=="n_second_field" or element.name=="n_second_label" then
-            item_requester.top.n_second_flow.n_second_radio.state=true
-            item_requester.top.n_count_flow .n_count_radio .state=false
-            setting.mode = "n_second"
-            update_gui(player)
-        elseif element.name=="n_count_radio" or element.name=="n_count_field" or element.name=="n_count_label" then
-            item_requester.top.n_second_flow.n_second_radio.state=false
-            item_requester.top.n_count_flow .n_count_radio .state=true
-            setting.mode = "n_count"
-            update_gui(player)
-        elseif element.name=="buffer_checkbox" then
-            setting.buffer_request = item_requester.top.buffer_flow.buffer_checkbox.state
-            update_gui(player)
+        local element = event.element
+        if element and element.valid then
+            if element.name=="n_second_radio" or element.name=="n_second_field" or element.name=="n_second_label" then
+                item_requester.top.n_second_flow.n_second_radio.state=true
+                item_requester.top.n_count_flow .n_count_radio .state=false
+                setting.mode = "n_second"
+                update_gui(player)
+            elseif element.name=="n_count_radio" or element.name=="n_count_field" or element.name=="n_count_label" then
+                item_requester.top.n_second_flow.n_second_radio.state=false
+                item_requester.top.n_count_flow .n_count_radio .state=true
+                setting.mode = "n_count"
+                update_gui(player)
+            elseif element.name=="buffer_checkbox" then
+                setting.buffer_request = item_requester.top.buffer_flow.buffer_checkbox.state
+                update_gui(player)
+            end
         end
+    end)
+    if not success then
+        reset()
+        error_message(message)
     end
 end
 
 function on_gui_text_changed(event)
-    local player_index = event.player_index
-    local player = game.players[player_index]
-    local item_requester = player.gui.left.item_requester
-    if not item_requester then return end
+    local success,message = pcall(function ()
+        local player_index = event.player_index
+        local player = game.players[player_index]
+        local item_requester = player.gui.left.item_requester
+        if not item_requester then return end
 
-    local setting = get_setting(player)
+        local setting = get_setting(player)
 
-    local element = event.element
-    if element and element.valid then
-        if element.name=="n_second_field" then
-            setting.n_second = tonumber(item_requester.top.n_second_flow.n_second_field.text) or 0
-            if setting.n_second ~= 0 then update_gui(player) end
-        elseif element.name=="n_count_field" then
-            setting.n_count  = tonumber(item_requester.top.n_count_flow .n_count_field .text) or 0
-            if setting.n_count  ~= 0 then update_gui(player) end
+        local element = event.element
+        if element and element.valid then
+            if element.name=="n_second_field" then
+                setting.n_second = tonumber(item_requester.top.n_second_flow.n_second_field.text) or 0
+                if setting.n_second ~= 0 then update_gui(player) end
+            elseif element.name=="n_count_field" then
+                setting.n_count  = tonumber(item_requester.top.n_count_flow .n_count_field .text) or 0
+                if setting.n_count  ~= 0 then update_gui(player) end
+            end
         end
+    end)
+    if not success then
+        reset()
+        error_message(message)
     end
 end
 
@@ -384,8 +415,13 @@ function reset()
         if item_requester then item_requester.destroy() end
     end
     global.settings=nil
-        
 end
+
+function error_message(message)
+    game.print{"","[",{"mod-name.DistributionRequestPlanner"}," ",script.active_mods.DistributionRequestPlanner,"] ",{"distribution-request-planner.error_message"}}
+    game.print(message)
+end
+
 function on_configuration_changed()
     reset()
 end
